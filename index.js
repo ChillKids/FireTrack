dd76f4080191b50a42ef1a788b4c203446d7f325
JackKit <chillkit@users.noreply.github.com>
2019-11-23 23:18:15 -0700

    get order json

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const { Pool } = require('pg')
 
 const connectionString = process.env.DATABASE_URL || "postgres://edjrblcydonapn:42e1f69ee902115d76f4acf357a35cb5576916a4417bf172c59565e6c8e12624@ec2-174-129-214-42.compute-1.amazonaws.com:5432/d7oveiplirdm04?ssl=true";
 const pool = new Pool({ connectionString: connectionString });
@@ -10,9 +11,10 @@ express()
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
+    .get('/getOrder', getOrder)
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-.get('/getOrder', (req, res) => {
+function getOrder(req, res) {
     var sql = "SELECT * FROM order_list";
     console.log("result.rows");
     pool.query(sql, function(err, result) {
@@ -25,6 +27,7 @@ express()
         // Log this to the console for debugging purposes.
         console.log("Back from DB with result:");
         console.log(result.rows);
+        res.json(result.rows);
     });
 
-})
+}
